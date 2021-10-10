a10d46bde83523aa78d1404dbfa0c91eea5ea030
Stackbit Code Editor <projects@stackbit.com>
2021-10-10 21:13:58 +0000

    gatsby-ssr.js: changed

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -15,7 +15,7 @@ exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
 
     setPostBodyComponents([
         <React.Fragment>
-            <script src={withPrefix('js/plugins.js')} defer/>
+            <script src={withPrefix('js/plugins.js')}/>
             <script src={withPrefix('js/init.js')} defer/>
             <script src={withPrefix('js/main.js')} defer/>
         </React.Fragment>
