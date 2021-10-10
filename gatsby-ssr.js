abc820a83640cf24b52ac565127e0cf68104a51f
Stackbit Code Editor <projects@stackbit.com>
2021-10-10 21:04:15 +0000

    gatsby-ssr.js: changed

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -18,10 +18,6 @@ exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
             <script src={withPrefix('js/plugins.js')} defer/>
             <script src={withPrefix('js/init.js')} defer/>
             <script src={withPrefix('js/main.js')} defer/>
-            <div id="fb-root"></div>
-                  <div class="fb-customerchat" attribution="biz_inbox" page_id="121349286117840" data-lazy="true" greeting_dialog_display="icon"></div>
-            <script>window.fbAsyncInit=function(){FB.init({xfbml:!0,version:"v10.0"})};</script>
-            <script src="https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js" async defer></script>
         </React.Fragment>
     ]);
 
