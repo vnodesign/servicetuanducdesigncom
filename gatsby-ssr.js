afb3d3f0fce134c8ac5b31844e5f75ca8ef6da68
Stackbit Code Editor <projects@stackbit.com>
2021-10-10 20:58:30 +0000

    gatsby-ssr.js: changed

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -21,7 +21,7 @@ exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
             <script>
             window.fbAsyncInit=function(){FB.init({xfbml:!0,version:"v10.0"})};function td_customerchat(){var t=document.createElement("script");t.async=!0,t.defer=!0,t.src="https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js",document.body.appendChild(t)}window.addEventListener?window.addEventListener("load",td_customerchat,!1):window.attachEvent?window.attachEvent("onload",td_customerchat):window.onload=td_customerchat;
             window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','UA-87140673-18');function td_gtag(){var t=document.createElement("script");t.async=!0,t.defer=!0,t.src="https://www.googletagmanager.com/gtag/js?id=UA-87140673-18",document.body.appendChild(t)}window.addEventListener?window.addEventListener("load",td_gtag,!1):window.attachEvent?window.attachEvent("onload",td_gtag):window.onload=td_gtag;
-    </script>
+            </script>
 
         </React.Fragment>
     ]);
