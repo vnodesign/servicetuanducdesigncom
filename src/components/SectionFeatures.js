ec338c77b555d3a1c4ff85143d0d633aea34412a
Stackbit Code Editor <projects@stackbit.com>
2021-10-10 20:46:30 +0000

    SectionFeatures.js: changed

--- src/components/SectionFeatures.js
+++ src/components/SectionFeatures.js
@@ -29,7 +29,7 @@ export default class SectionFeatures extends React.Component {
                       <img src={withPrefix(_.get(feature, 'image', null))} alt={_.get(feature, 'image_alt', null)} />
                     </div>
                     )}
-                    <div className="cell block-content wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s">
+                    <div className="cell block-content">
                       <h3 className="block-title underline">{_.get(feature, 'title', null)}</h3>
                       <div className="block-copy">
                         {markdownify(_.get(feature, 'content', null))}
