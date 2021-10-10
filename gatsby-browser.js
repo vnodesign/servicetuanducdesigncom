f320f301a5043b52a180ec08d9401d9e9d9e29bb
Stackbit Code Editor <projects@stackbit.com>
2021-10-10 20:54:04 +0000

    gatsby-browser.js: changed

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -31,4 +31,19 @@
 //         window.onGatsbyPreRouteUpdate();
 //     }
 // };
+/* eslint-disable no-alert */
+/* eslint-disable import/prefer-default-export */
 
+'use strict';
+
+require('./src/assets/scss/init.scss');
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    'This blog has been updated. '
+      + 'Reload to display the latest version?'
+  );
+  if (answer === true) {
+    window.location.reload();
+  }
+};
