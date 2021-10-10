d6420fc086487d52d7548b80d1b520dbba8ef363
Stackbit Code Editor <projects@stackbit.com>
2021-10-10 20:28:21 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -16,7 +16,7 @@ export default class SectionHero extends React.Component {
                     <img src={withPrefix(_.get(section, 'image', null))} alt={_.get(section, 'image_alt', null)} />
                   </div>
                   )}
-                  <div className="cell block-content ">
+                  <div className="cell block-content wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s">
                     {_.get(section, 'title', null) && (
                     <h2 className="block-title underline">{_.get(section, 'title', null)}</h2>
                     )}
