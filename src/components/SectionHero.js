17633c1d4b06f9b9ccbf358ff839b63f879705bc
Stackbit Code Editor <projects@stackbit.com>
2021-10-15 10:09:52 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -11,28 +11,28 @@ export default class SectionHero extends React.Component {
             <section id={_.get(section, 'section_id', null)} className="block">
             <div className="hero-block">
                 <div className="bg-accent">
-              <div className="inner">
-                <div className="grid">
-                  {_.get(section, 'image', null) && (
-                  <div className="cell block-preview">
-                    <img src={withPrefix(_.get(section, 'image', null))} alt={_.get(section, 'image_alt', null)} />
-                  </div>
-                  )}
-                  <div className="cell block-content">
-                    {_.get(section, 'title', null) && (
-                    <h2 className="block-title underline">{_.get(section, 'title', null)}</h2>
-                    )}
-                    <div className="block-copy">
-                      {markdownify(_.get(section, 'content', null))}
+                    <div className="inner">
+                        <div className="grid">
+                        {_.get(section, 'image', null) && (
+                        <div className="cell block-preview">
+                            <img src={withPrefix(_.get(section, 'image', null))} alt={_.get(section, 'image_alt', null)} />
+                        </div>
+                        )}
+                        <div className="cell block-content">
+                            {_.get(section, 'title', null) && (
+                            <h2 className="block-title underline">{_.get(section, 'title', null)}</h2>
+                            )}
+                            <div className="block-copy">
+                            {markdownify(_.get(section, 'content', null))}
+                            </div>
+                            {_.get(section, 'actions', null) && (
+                            <div className="block-buttons">
+                            <CtaButtons {...this.props} actions={_.get(section, 'actions', null)} />
+                            </div>
+                            )}
+                        </div>
+                        </div>
                     </div>
-                    {_.get(section, 'actions', null) && (
-                    <div className="block-buttons">
-                      <CtaButtons {...this.props} actions={_.get(section, 'actions', null)} />
-                    </div>
-                    )}
-                  </div>
-                </div>
-              </div>
             </div>
             </div>
             </section>
