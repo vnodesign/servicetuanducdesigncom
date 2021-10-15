291d9e5acf12fa3e7a3bbb47b005bebbb5b53951
Stackbit Code Editor <projects@stackbit.com>
2021-10-15 10:24:21 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
-
+import { LazyLoadComponent } from 'react-lazy-load-image-component';
 import {withPrefix, markdownify} from '../utils';
 import CtaButtons from './CtaButtons';
 
@@ -8,41 +8,31 @@ export default class SectionHero extends React.Component {
     render() {
         let section = _.get(this.props, 'section', null);
         return (
-            <section id={_.get(section, 'section_id', null)} className="block">
-            <div className="hero-block">
-                <div className="bg-accent">
-                    <div className="outer">
-                        <div className="inner">
-                            <div className="grid">
-                            {_.get(section, 'image', null) && (
-                            <div className="cell">
-                                <div className="block-preview">
-                                    <img src={withPrefix(_.get(section, 'image', null))} alt={_.get(section, 'image_alt', null)} />
-                                </div>
-                            </div>
-                            )}
-                            <div className="cell">
-                                <div className="block-content">
-                                    {_.get(section, 'title', null) && (
-                                    <div className="block-title">
-                                        <h2 className="underline">{_.get(section, 'title', null)}</h2>
-                                    </div>
-                                    )}
-                                    <div className="block-copy">
-                                    {   markdownify(_.get(section, 'content', null))}
-                                    </div>
-                                    {_.get(section, 'actions', null) && (
-                                    <div className="block-buttons">
-                                        <CtaButtons {...this.props} actions={_.get(section, 'actions', null)} />
-                                    </div>
-                                    )}
-                                </div>
-                            </div>
-                            </div>
-                        </div>
+            <section id={_.get(section, 'section_id', null)} className="block hero-block bg-accent outer">
+              <div className="inner">
+                <div className="grid">
+                  {_.get(section, 'image', null) && (
+                  <div className="cell block-preview">
+                  <LazyLoadComponent>
+                    <img src={withPrefix(_.get(section, 'image', null))} alt={_.get(section, 'image_alt', null)} loading="lazy" decoding="async" />
+                  </LazyLoadComponent>
+                  </div>
+                  )}
+                  <div className="cell block-content">
+                    {_.get(section, 'title', null) && (
+                    <h2 className="block-title underline">{_.get(section, 'title', null)}</h2>
+                    )}
+                    <div className="block-copy">
+                      {markdownify(_.get(section, 'content', null))}
                     </div>
+                    {_.get(section, 'actions', null) && (
+                    <div className="block-buttons">
+                      <CtaButtons {...this.props} actions={_.get(section, 'actions', null)} />
+                    </div>
+                    )}
+                  </div>
                 </div>
-            </div>
+              </div>
             </section>
         );
     }
