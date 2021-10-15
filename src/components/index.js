3fa51dcd7580804cdf810979244b5ecdcd614459
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2021-10-15 20:33:42 +0700

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,42 +1,25 @@
-import loadable from '@loadable/component';
 import Action from './Action';
 import ActionLink from './ActionLink';
-//import BlogPostFooter from './BlogPostFooter';
 import CtaButtons from './CtaButtons';
-const Footer = loadable(() =>
-  import(/* webpackPrefetch: true */ './Footer')
-);
-//import Footer from './Footer';
+import Footer from './Footer';
 import FooterForm from './FooterForm';
 import FooterNav from './FooterNav';
-const FooterText = loadable(() =>
-  import(/* webpackPrefetch: true */ './FooterText')
-);
-//import FooterText from './FooterText';
+import FooterText from './FooterText';
 import FormField from './FormField';
 import Header from './Header';
 import Icon from './Icon';
 import SectionContact from './SectionContact';
-const SectionContent = loadable(() =>
-  import(/* webpackPrefetch: true */ './SectionContent')
-);
-//import SectionContent from './SectionContent';
+import SectionContent from './SectionContent';
 import SectionCta from './SectionCta';
 import SectionFaq from './SectionFaq';
-const SectionFeatures = loadable(() =>
-  import(/* webpackPrefetch: true */ './SectionFeatures')
-);
-//import SectionFeatures from './SectionFeatures';
+import SectionFeatures from './SectionFeatures';
 import SectionHero from './SectionHero';
-//import SectionPosts from './SectionPosts';
 import SectionPricing from './SectionPricing';
-//import SectionReviews from './SectionReviews';
 import Layout from './Layout';
 
 export {
     Action,
     ActionLink,
-    //BlogPostFooter,
     CtaButtons,
     Footer,
     FooterForm,
@@ -51,16 +34,13 @@ export {
     SectionFaq,
     SectionFeatures,
     SectionHero,
-    //SectionPosts,
     SectionPricing,
-    //SectionReviews,
     Layout
 };
 
 export default {
     Action,
     ActionLink,
-    //BlogPostFooter,
     CtaButtons,
     Footer,
     FooterForm,
@@ -75,8 +55,6 @@ export default {
     SectionFaq,
     SectionFeatures,
     SectionHero,
-    //SectionPosts,
     SectionPricing,
-    //SectionReviews,
     Layout
 };
