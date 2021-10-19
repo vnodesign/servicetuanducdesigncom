643e24b2ce7cd875cede1268c9d07ce83c63708b
Stackbit Code Editor <projects@stackbit.com>
2021-10-19 07:13:01 +0000

    SectionContact.js: changed

--- src/components/SectionContact.js
+++ src/components/SectionContact.js
@@ -46,7 +46,7 @@ export default class SectionContact extends React.Component {
                   <div className="form-row form-submit">
                   {status === "SUCCESS" ? <p>Thông tin của bạn đã được gửi đi, đừng quên kiểm tra email của bạn để xem có tin nhắn từ chúng tôi không nhé!</p> : <button type="submit" className="button">{_.get(section, 'submit_label', null)}</button>}
                   {status === "ERROR" && <p>Xin lỗi! Bạn chưa nhập đẩy đủ thông tin hoặc hệ thống đang bị lỗi, vui lòng thử lại sau.</p>}
-                </div>
+                    </div>
                 </form>
               </div>
             </section>
